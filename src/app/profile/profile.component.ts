a06909998430542ed6b95bab3c5efb92600795f0
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-19 20:56:59 +0530

    learn: learn with ngfor Directive

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,11 +1,44 @@
 import { Component } from "@angular/core";
+import { NgForOf } from "../../../node_modules/@angular/common/common_module.d-NEF7UaHr";
+import { NgFor } from "@angular/common";
 
 @Component({
   selector:'app-profile',
   // template:`<h1>profile Compoent</h1>`,
   templateUrl:'./profile.component.html',
+  imports: [NgFor],
 })
-
+   
 export class ProfileCompoent{
+ students=["vaibhav","aniket","rahil","vishal","peter"]
 
+ studentData=[
+  {
+    name:'vaibhav',
+    age:'21',
+    email:'vaibhav@test.com'
+  },
+  {
+    name:'aniket',
+    age:'24',
+    email:'aniket@test.com'
+  },
+  {
+    name:'rahil',
+    age:'26',
+    email:'rahil@test.com'
+  },
+  {
+    name:'vishal',
+    age:'28',
+    email:'vishal@test.com'
+  },
+  {
+    name:'peter',
+    age:'30',
+    email:'peter@test.com'
+  }
+ ]
+
+ 
 }
