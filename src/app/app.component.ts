d92a31296dad0ceab712e9b22f94ed42145c0268
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-20 13:28:49 +0530

    learn: learn about ngFor structural diractive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,4 +18,5 @@ import { ProjectComponent } from "./project/project.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {}
+export class AppComponent {
+}
