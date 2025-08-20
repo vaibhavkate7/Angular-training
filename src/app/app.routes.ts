984e0285af5d7374aad6efee28a995ef1a0a9f2d
vaibhavkate7 <katevaibhav417@gmail.com>
2025-08-20 18:13:39 +0530

    learn: learn about 4o4 Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { ProfileCompoent } from './profile/profile.component';
 import { ProjectComponent } from './project/project.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'profile',component:ProfileCompoent},
   {path:'project',component:ProjectComponent},
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'**',component:PageNotFoundComponent}
 ];
